2ade0991208e76d451cc648d7aed00f01045629a
kendallcorner <kmwerts@gmail.com>
2022-03-22 19:54:57 -0500

    v 1.5 sitters

--- seeking-sitters.user.js
+++ seeking-sitters.user.js
@@ -3,7 +3,7 @@
 // @author         kendallcorner
 // @copyright 2022
 // @license GNU General Public License version 2.0
-// @version        1.4
+// @version        1.5
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require https://gist.githubusercontent.com/BrockA/2625891/raw/9c97aa67ff9c5d56be34a55ad6c18a314e5eb548/waitForKeyElements.js
 // @include https://www.seekingsitters.com/members/memberrequest
@@ -14,7 +14,7 @@ const runOnSite = () => {
     const tabs = document.getElementsByClassName('nav nav-tabs')
     tabs[1].children[2].children[0].click()
     document.getElementById('ddStartTimeGROUP').value='17:30:00'
-    document.getElementById('ddEndTimeGROUP').value='20:30:00'
+    document.getElementById('ddEndTimeGROUP').value='21:30:00'
     document.getElementById('CommentsToAdminGROUP').value='Vaccinated sitters, please'
     document.getElementById('CommentsToSitterGROUP').value='Vaccinated sitters, please'
 };
